06a6688fdee40257063e9b4ccaa3fb25cb1342f5
Neha Bhanudas Chaudhari <neha.chaudhari@publicissapient.com>
2020-09-24 23:10:45 +0530

    added new file

--- src/main/java/com/example/user/App.java
+++ src/main/java/com/example/user/App.java
@@ -3,8 +3,16 @@ package com.example.user;
 
 public class App 
 {
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Can you see me on Jenkins?" );
+        System.out.println( "Hello World! Can you see me on Jenkins?" );        
+        
     }
+
 }
